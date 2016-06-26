fb97342a32c28fe653ac1e3729d54ac45e81b912
Clarence Mills <cmills@opensitesolutions.com>
2016-06-26 16:07:16 -0400

    New files variables.tf that points to terraform.tfvars which holds token, not added to repository by .gitignore

---
+++ digitalocean/droplet.tf
@@ -0,0 +1,15 @@
+# Set the variable value in *.tfvars file
+# or using -var="digtoken=..." CLI option
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+    token = "${var.digitaloceantoken}"
+}
+
+# Create a new Web droplet in the nyc2 region
+resource "digitalocean_droplet" "test" {
+    image = "ubuntu-14-04-x64"
+    name = "terraform-test"
+    region = "nyc2"
+    size = "512mb"
+}
---
+++ digitalocean/variables.tf
@@ -0,0 +1,3 @@
+variable "digitaloceantoken" {
+	digitaloceantoken = ""
+}
